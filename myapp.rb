ab997f999cc2766ba3738585bb017822ce8a03ac
zombie_chan <aloskdjfhg@gmail.com>
2012-04-06 03:56:59 -0700

    updated.

---
+++ myapp.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'sinatra'
+
+helpers do
+  include Rack::Utils; alias_method :h, :escape_html
+end
+
+template :layout do
+  "<html><body><h1>Hello World</h1><%= yield %></body></html>"
+end
+
+get '/' do
+  erb %{
+    <p>あなたの名前は？</p>
+    <form action='/hello' method='POST'>
+    <input type='text' name='name'>
+    <br>
+    <input type='submit' value='送信'>
+    </form>
+  }
+end
+
+post '/hello' do
+  erb :index
+end
